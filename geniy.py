7b0c3ccaaea448be61fbda5f4b3c345ba41bc58e
halker1907 <113712157+halker1907@users.noreply.github.com>
2022-10-07 19:47:15 +0300

    Add files via upload

---
+++ geniy.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.shape("turtle")
+turtle.color("red")
+
+counter = 0
+side = 50
+
+while counter < 4:
+    turtle.forward(side)
+    turtle.left(90)
+    counter += 1
+
+turtle.left(90)
+turtle.forward(50)
+turtle.right(45)
+turtle.fd(40)
+turtle.right(100)
+turtle.fd(40)
+turtle.right(38)
+turtle.fd(48)
+turtle.right(90)
+turtle.fd(17)
+
+
+
+
+
+turtle.done()
---
+++ turtlenik.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.shape("turtle")
+turtle.color("red")
+
+counter = 0
+side = 50
+speed = 100000000000000000000000
+while counter < 4:
+    turtle.forward(side)
+    turtle.left(90)
+    counter += 1
+
+
+
+turtle.done()
+
