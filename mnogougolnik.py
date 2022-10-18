230aa530e400ac3fcb36e2aaa964d21914392f01
halker1907 <113712157+halker1907@users.noreply.github.com>
2022-10-18 19:41:04 +0300

    Add files via upload

---
+++ mnogougolnik.py
@@ -0,0 +1,30 @@
+import turtle as t
+import random
+
+t.speed(0)
+t.colormode(255)
+
+while True:
+	x = random.randint(-300,300)
+	y = random.randint(-200,200)
+	
+	sides = random.randint(3,10)
+	red = random.randint(0,255)
+	green = random.randint(0,255)
+	blue = random.randint(0,255)
+
+	t.penup()
+	t.goto(x,y)
+
+	t.pendown()
+	t.fillcolor(red,green,blue)
+	t.begin_fill()
+	for i in range(sides):
+		t.fd(360 / sides * 2)
+		t.left(360 / sides)
+	t.end_fill()
+
+
+
+
+t.down()
--- snow_man1.py
+++ snow_man1.py
@@ -1,15 +1,33 @@
 import turtle as t
+import random
 
-start_radios = 50
+t.speed(0)
 
-for i in range(3):
+while True:
+    t.colormode(255)
+    x = random.randint(-200, 200)
+    y = random.randint(-100, 100)
+    circle = random.randint(3,6)
+    radios = random.randint(-100, 100)
+    red = random.randint(0, 255)
+    green = random.randint(0, 255) 
+    blue = random.randint(0, 255)
 
-    t.circle(start_radios * -1)
     t.penup()
-    t.
-    #доделать
+    t.setposition(x,y)
+    t.pendown()
 
-
-
+    for i in range(3):
+        t.pendown()
+        t.fillcolor(red,green,blue)
+        t.begin_fill()
+        t.circle(radios)
+        t.end_fill()
+        t.penup()
+        t.setheading(90)
+        t.fd(radios * 2)
+        t.setheading(0)
+        radios = radios / 2
+        
 
 t.down()
